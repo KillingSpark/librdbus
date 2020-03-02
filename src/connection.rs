733fcb06a075a4b334a4580c3addea2ad39d2d10
Moritz Borcherding <moritz.borcherding@web.de>
2020-03-02 15:29:39 +0100

    add blocking dbus calls

--- src/connection.rs
+++ src/connection.rs
@@ -11,6 +11,43 @@ pub enum ConState {
 
 pub struct DBusPreallocatedSend {}
 
+// TODO protect with mutex?
+pub struct DBusPendingCall<'a> {
+    serial: u32,
+    ref_count: u64,
+    timeout: Option<std::time::Duration>,
+    reply: Option<*mut DBusMessage<'a>>,
+    mutex: std::sync::Mutex<()>,
+    cond: std::sync::Condvar,
+}
+
+impl<'a> DBusPendingCall<'a> {
+    pub fn new(serial: u32, timeout: Option<std::time::Duration>) -> Self {
+        DBusPendingCall {
+            serial,
+            ref_count: 1,
+            reply: None,
+            timeout,
+            cond: std::sync::Condvar::new(),
+            mutex: std::sync::Mutex::new(()),
+        }
+    }
+
+    pub fn wait_for_reply(&mut self) -> Result<(), ()> {
+        if let Some(timeout) = self.timeout {
+            match self.cond.wait_timeout(self.mutex.lock().unwrap(), timeout) {
+                Err(_) => Err(()),
+                Ok(_) => Ok(()),
+            }
+        } else {
+            match self.cond.wait(self.mutex.lock().unwrap()) {
+                Err(_) => Err(()),
+                Ok(_) => Ok(()),
+            }
+        }
+    }
+}
+
 #[repr(C)]
 pub enum DBusDispatchStatus {
     Complete,
@@ -26,6 +63,8 @@ pub struct DBusConnection<'a> {
 
     out_queue: VecDeque<*mut DBusMessage<'a>>,
     in_queue: VecDeque<*mut DBusMessage<'a>>,
+
+    pending_calls: Vec<*mut DBusPendingCall<'a>>,
 }
 
 impl<'a> DBusConnection<'a> {
@@ -37,6 +76,7 @@ impl<'a> DBusConnection<'a> {
             exit_on_disconnect: false,
             out_queue: VecDeque::new(),
             in_queue: VecDeque::new(),
+            pending_calls: Vec::new(),
         }
     }
 
@@ -47,6 +87,25 @@ impl<'a> DBusConnection<'a> {
                 self.con.send_message(&mut msg.msg, timeout).unwrap();
             }
         }
+    }
+
+    pub fn dispatch_message(&mut self, msg: DBusMessage<'a>) {
+        if let rustbus::MessageType::Reply = msg.msg.typ {
+            if let Some(reply_serial) = msg.msg.response_serial {
+                for p in &mut self.pending_calls {
+                    let p = unsafe { &mut **p };
+                    if p.serial == reply_serial {
+                        p.reply = Some(Box::into_raw(Box::new(msg)));
+                        p.cond.notify_all();
+                        return;
+                    }
+                }
+            }
+        }
+
+        // TODO implement handlers / fallbacks for object paths
+
+        self.in_queue.push_back(Box::into_raw(Box::new(msg)))
     }
 }
 
@@ -124,11 +183,11 @@ pub extern "C" fn dbus_connection_send<'a>(
     serial: *mut u32,
 ) -> u32 {
     if con.is_null() {
-        return 0;
+        return dbus_bool(false);
     }
     let con = unsafe { &mut *con };
     if msg.is_null() {
-        return 0;
+        return dbus_bool(false);
     }
     let msg = unsafe { &mut *msg };
     let new_serial = con.con.alloc_serial();
@@ -271,14 +330,81 @@ pub extern "C" fn dbus_connection_dispatch(con: *mut DBusConnection) -> DBusDisp
             Err(_e) => {
                 // TODO
             }
-            Ok(msg) => con
-                .in_queue
-                .push_back(Box::into_raw(Box::new(DBusMessage::new(msg)))),
+            Ok(msg) => con.dispatch_message(DBusMessage::new(msg)),
         }
     }
     if con.con.can_read_from_source().unwrap() {
         DBusDispatchStatus::DataRemaining
     } else {
         DBusDispatchStatus::Complete
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn dbus_connection_send_with_reply<'a>(
+    con: *mut DBusConnection<'a>,
+    msg: *mut DBusMessage<'a>,
+    pending: *mut *mut DBusPendingCall,
+    timeout: libc::c_int,
+) -> u32 {
+    if con.is_null() {
+        return dbus_bool(false);
+    }
+    let con = unsafe { &mut *con };
+    if msg.is_null() {
+        return dbus_bool(false);
+    }
+    let msg = unsafe { &mut *msg };
+    if pending.is_null() {
+        return dbus_bool(false);
+    }
+    let pending = unsafe { &mut *pending };
+
+    let mut serial = 0u32;
+    if dbus_connection_send(con, msg, &mut serial) == dbus_bool(false) {
+        return dbus_bool(false);
+    }
+
+    let timeout = if timeout < 0 {
+        // TODO what does libdbus use here as a 'sane' default (as they call it)?
+        Some(std::time::Duration::from_millis(10))
+    } else {
+        Some(std::time::Duration::from_millis(timeout as u64))
+    };
+
+    *pending = Box::into_raw(Box::new(DBusPendingCall::new(serial, timeout)));
+    dbus_bool(true)
+}
+#[no_mangle]
+pub extern "C" fn dbus_connection_send_with_reply_and_block<'a>(
+    con: *mut DBusConnection<'a>,
+    msg: *mut DBusMessage<'a>,
+    timeout: libc::c_int,
+    err: *mut DBusError,
+) -> *mut DBusMessage<'a> {
+    if con.is_null() {
+        return std::ptr::null_mut();
+    }
+    let con = unsafe { &mut *con };
+    if msg.is_null() {
+        return std::ptr::null_mut();
+    }
+    let msg = unsafe { &mut *msg };
+    let pending = std::ptr::null_mut();
+    dbus_connection_send_with_reply(con, msg, pending, timeout);
+
+    // TODO convert error replys to DBusError
+    let _ = err;
+
+    let pending = unsafe { &mut **pending };
+    match pending.wait_for_reply() {
+        Ok(()) => {
+            if let Some(reply) = pending.reply {
+                reply
+            } else {
+                std::ptr::null_mut()
+            }
+        }
+        Err(()) => std::ptr::null_mut(),
     }
 }
